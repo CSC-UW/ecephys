ab29e48d649afba76962a963ae22c0c3511a8ad6
Graham Findlay <graham.b.findlay@gmail.com>
2020-12-24 18:01:32 -0600

    switched to datapaths csv

--- ecephys/data/paths.py
+++ ecephys/data/paths.py
@@ -1,85 +1,9 @@
-lfp_bin = {
-    "SR": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t23.imec0.lf.bin",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-20-2020_g0_t3.imec0.lf.bin",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t3.imec0.lf.bin",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-25-2020_g0_t2.imec0.lf.bin",
-        "Eugene": "/Volumes/neuropixel/Data/CNPIX6-Eugene/9.24.2020_SR_24hs_g0_t0.imec0.lf.bin",
-    },
-    "BL": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t11.imec0.lf.bin",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-19-2020_g1_t3.imec0.lf.bin",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-17-2020_g0_t3.imec0.lf.bin",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-24-2020_g0_t6.imec0.lf.bin",
-        "Eugene": "/Volumes/neuropixel/Data/CNPIX6-Eugene/9.24.2020_BL_24hs2_g0_t5.imec0.lf.bin",
-    },
-    "pre-SR": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t22.imec0.lf.bin",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-20-2020_g0_t2.imec0.lf.bin",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t2.imec0.lf.bin",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-25-2020_g0_t1.imec0.lf.bin",
-    },
-}
+import os
+import pandas as pd
 
-hypnograms = {
-    "SR": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t23.hypnogram.txt",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-20-2020_g0_t3.hypnogram.txt",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t3.hypnogram.txt",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-25-2020_g0_t2.hypnogram.txt",
-        "Eugene": "/Volumes/neuropixel/Data/CNPIX6-Eugene/9.24.2020_SR_24hs_g0_t0.hypnogram.txt",
-    },
-    "BL": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t11.hypnogram.txt",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-19-2020_g1_t3.hypnogram.txt",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-17-2020_g0_t3.hypnogram.txt",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-24-2020_g0_t6.hypnogram.txt",
-        "Eugene": "/Volumes/neuropixel/Data/CNPIX6-Eugene/9.24.2020_BL_24hs2_g0_t5.hypnogram.txt",
-    },
-    "pre-SR": {},
-}
+MODULE_DIRECTORY = os.path.dirname(os.path(abspath(__file__)))
+DATAPATHS_FILE = os.path.join(MODULE_DIRECTORY, "datapaths.csv")
 
-ripples = {
-    "SR": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t23.imec0.ripples.h5",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-20-2020_g0_t3.imec0.ripples.h5",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t3.imec0.ripples.h5",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-25-2020_g0_t2.imec0.ripples.h5",
-        "Eugene": "/Volumes/neuropixel/Data/CNPIX6-Eugene/9.24.2020_SR_24hs_g0_t0.imec0.ripples.h5",
-    },
-    "BL": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t11.imec0.ripples.h5",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-19-2020_g1_t3.imec0.ripples.h5",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-17-2020_g0_t3.imec0.ripples.h5",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-24-2020_g0_t6.imec0.ripples.h5",
-        "Eugene": "/Volumes/neuropixel/Data/CNPIX6-Eugene/9.24.2020_BL_24hs2_g0_t5.imec0.ripples.h5",
-    },
-    "pre-SR": {
-        "Segundo": "/Volumes/neuropixel/Data/CNPIX2-Segundo/1-21-2020_g0_t22.imec0.ripples.h5",
-        "Valentino": "/Volumes/neuropixel/Data/CNPIX3-Valentino/2-20-2020_g0_t2.imec0.ripples.h5",
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t2.imec0.ripples.h5",
-        "Alessandro": "/Volumes/neuropixel/Data/CNPIX5-Alessandro/8-25-2020_g0_t1.imec0.ripples.h5",
-    },
-}
 
-spws = {
-    "SR": {
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t3.imec0.spws.h5",
-    },
-    "BL": {
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-17-2020_g0_t3.imec0.spws.h5",
-    },
-    "pre-SR": {
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t2.imec0.spws.h5",
-    },
-}
-
-emg = {
-    "SR": {
-        "Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t3.imec0.emg.nc"
-    }
-}
-
-lfp_bandpower = {
-    "SR": {"Doppio": "/Volumes/neuropixel/Data/CNPIX4-Doppio/3-18-2020_g0_t3.pow.nc"}
-}
+def load_datapaths():
+    df = pd.load(DATAPATHS_FILE)
