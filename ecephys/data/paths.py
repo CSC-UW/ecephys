6fb1925c3ec63ab0a7e33c4a901ba63da18ef05c
Tom Bugnon <tombugnon@hotmail.com>
2021-03-25 08:02:16 -0500

    Handle paths for catGT-concatenated data

--- ecephys/data/paths.py
+++ ecephys/data/paths.py
@@ -60,46 +60,93 @@ def parse_sglx_stem(stem):
     return (run, gate, trigger, probe)
 
 
-def get_sglx_style_filename(run, gate, trigger, probe, ext):
+def get_sglx_style_filename(run, gate, trigger, probe, ext, catgt_data=False):
     """Get SpikeGLX-style filename from parts.
     Note that ext is of the form `lf.bin`, not `.lf.bin`.
     """
-    return f"{run}_{gate}_{trigger}.{probe}.{ext}"
+    trig = trigger if not catgt_data else "tcat"
+    return f"{run}_{gate}_{trig}.{probe}.{ext}"
 
 
-def get_sglx_style_parent_path(run, gate, trigger, probe, root_dir):
+def get_sglx_style_parent_path(run, gate, trigger, probe, root_dir, catgt_data=False):
     """Get the parent path where an SpikeGLX file would be found, assuming
     folder-per-probe organization.
     """
     probe_dir = f"{run}_{gate}_{probe}"
-    run_dir = f"{run}_{gate}"
+    run_dir = f"{run}_{gate}" if not catgt_data else f"catgt_{run}_{gate}"
     return root_dir / run_dir / probe_dir
 
 
-def get_sglx_style_abs_path(stem, ext, root):
+def get_sglx_style_abs_path(stem, ext, root, catgt_data=False):
     """Get the absolute path where a SpikeGLX filew ould be found, assuming
     folder-per-probe organization.
+
+    CatGT saves data with `catgt_` prepended to the run directory and `cat`
+    as trigger idx
     """
     run, gate, trigger, probe = parse_sglx_stem(stem)
-    fname = get_sglx_style_filename(run, gate, trigger, probe, ext)
-    parent = get_sglx_style_parent_path(run, gate, trigger, probe, root)
+    fname = get_sglx_style_filename(
+        run, gate, trigger, probe, ext, catgt_data=catgt_data
+    )
+    parent = get_sglx_style_parent_path(
+        run, gate, trigger, probe, root, catgt_data=catgt_data
+    )
     return parent / fname
 
 
-def get_sglx_style_datapaths(yaml_path, subject, condition, ext):
+def get_sglx_style_datapaths(yaml_path, subject, condition, ext, catgt_data=False, cat_trigger=False):
     """Get all datapaths, assuming a properly formatted YAML file an folder-per-probe
-    organization."""
+    organization.
+    
+    Kwargs:
+        use_analysis_root: bool
+            Use analysis directory rather than raw data directory as root for returned path
+        catgt_data: bool
+            Path to corresponding catGT-processed concatenated file.
+            catGT file is saved in the analysis directory, uses `cat`
+            as the trigger index, and has `catgt_` prepended to the
+            run directory.
+        cat_trigger: bool
+            Replace trigger id with 'cat' to designate data issued from concatenated files.
+        
+    """
     with open(yaml_path) as fp:
         yaml_data = yaml.safe_load(fp)
 
-    if (ext == "lf.bin") or (ext == "ap.bin"):
+    data_file = (ext == "lf.bin") or (ext == "ap.bin")
+    if data_file and not catgt_data:
         root = Path(yaml_data[subject]["raw-data-root"])
     else:
         root = Path(yaml_data[subject]["analysis-root"])
 
-    condition_manifest = list(flatten(yaml_data[subject][condition]))
+    condition_data = yaml_data[subject][condition]
+    if isinstance(condition_data, dict):
+        # subject: condition: experiment_id: [stem_0, stem_1]
+        # Append experiment id to root
+        paths = []
+        for experiment_id in condition_data.keys():
+            condition_manifest = list(flatten(condition_data[experiment_id]))
+            experiment_root = root / experiment_id
+            paths += [
+                get_sglx_style_abs_path(
+                    stem, ext, experiment_root, catgt_data=catgt_data
+                )
+                for stem in condition_manifest
+            ]
+    else:
+        # subject: condition:[stem_0, stem_1]
+        condition_manifest = list(flatten(condition_data))
+        paths = [
+            get_sglx_style_abs_path(stem, ext, root, catgt_data=catgt_data)
+            for stem in condition_manifest
+        ]
 
-    return [get_sglx_style_abs_path(stem, ext, root) for stem in condition_manifest]
+    # For catGT data make sure all the condition files indeed concatenated
+    if catgt_data:
+        assert len(set(paths)) == 1
+        return list(set(paths))
+
+    return paths
 
 
 def get_datapath(yaml_path, subject, condition, file):
