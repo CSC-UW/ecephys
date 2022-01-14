c265e880dccf62f3fce97b9c9efe3eaa3f071bcf
Graham Findlay <graham.b.findlay@gmail.com>
2022-01-13 19:14:56 -0600

    Add CSD minima&variance estimation and plotting

--- ecephys/xrsig/kcsd.py
+++ ecephys/xrsig/kcsd.py
@@ -1,6 +1,9 @@
 import xarray as xr
 import numpy as np
 from kcsd import KCSD1D
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.signal import find_peaks
 
 
 def get_kcsd(sig, ele_pos, drop_chans=[], do_lcurve=False, **kcsd_kwargs):
@@ -49,3 +52,59 @@ def get_kcsd(sig, ele_pos, drop_chans=[], do_lcurve=False, **kcsd_kwargs):
         csd = csd.assign_coords({"channel": ("pos", channels)})
 
     return csd.assign_attrs(kcsd=k, fs=sig.fs)
+
+
+def get_epoched_minima(csd, epoch_length):
+    assert csd.time.values.max() >= epoch_length, "Epochs longer than data."
+    return csd.coarsen(
+        time=int(csd.fs * epoch_length), boundary="trim", coord_func={"time": "min"}
+    ).min()
+
+
+def get_epoched_variance(csd, epoch_length):
+    assert csd.time.values.max() >= epoch_length, "Epochs longer than data."
+    return csd.coarsen(
+        time=int(csd.fs * epoch_length), boundary="trim", coord_func={"time": "min"}
+    ).var()
+
+
+def plot_epoched_depth_profiles(da, figsize=(36, 8)):
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(
+        da,
+        xticklabels=da.time.values.round(),
+        yticklabels=da.channel.values,
+        cbar=False,
+    )
+    ax.set(
+        xticks=ax.get_xticks()[::2],
+        yticks=ax.get_yticks()[::4],
+        xlabel="Epoch center time (s)",
+        ylabel="Channel",
+    )
+    return fig, ax
+
+
+def plot_depth_profile(
+    da,
+    figsize=(36, 5),
+    ylabel=None,
+    mark_positive_peaks=False,
+    mark_negative_peaks=False,
+):
+    fig, ax = plt.subplots(figsize=figsize)
+    channel_indices = np.arange(len(da.channel))
+    sns.barplot(x=channel_indices, y=da, color="steelblue", ax=ax)
+    ax.set(xlabel="Channel", ylabel=ylabel)
+    ax.set_xticks(channel_indices)
+    ax.set_xticklabels(da.channel.values, rotation=90)
+
+    if mark_positive_peaks:
+        peaks, _ = find_peaks(da.values, prominence=1000, distance=10)
+        sns.scatterplot(x=peaks, y=da.values[peaks], marker="x", color="red", ax=ax)
+
+    if mark_negative_peaks:
+        peaks, _ = find_peaks(-da.values, prominence=1000, distance=10)
+        sns.scatterplot(x=peaks, y=da.values[peaks], marker="x", color="red", ax=ax)
+
+    return fig, ax
