acd0d8bd635aa30032912d60af556d935d457474
Graham Findlay <graham.b.findlay@gmail.com>
2022-03-27 21:57:57 -0500

    Add load_nidq_analog to sglx submodule

--- ecephys/sglx/__init__.py
+++ ecephys/sglx/__init__.py
@@ -2,3 +2,4 @@ from .utils import *
 from .cat_gt import *
 from .file_mgmt import *
 from .repair_sglx import *
+from .load_nidq_analog import *
---
+++ ecephys/sglx/load_nidq_analog.py
@@ -0,0 +1,64 @@
+import numpy as np
+import xarray as xr
+import sglxarray as sglxr
+from pathlib import Path
+from sglxarray.external.readSGLX import (
+    ChannelCountsNI,
+    readMeta,
+    SampRate,
+    makeMemMapRaw,
+    GainCorrectNI,
+)
+
+
+def is_channel_XA(meta, channel):
+    MN, MA, XA, DW = ChannelCountsNI(meta)
+    return (channel >= (MN + MA)) and (channel < XA)
+
+
+def load_nidq_analog(bin_path, channels, start_time=0, end_time=np.Inf):
+    # Read and validate the metadata
+    bin_path = Path(bin_path)
+    meta = readMeta(bin_path)
+
+    # Get the requested start and end samples
+    fs = SampRate(meta)
+    firstSamp = sglxr.sglxarray._to_seconds(start_time, meta) * fs
+    lastSamp = sglxr.sglxarray._to_seconds(end_time, meta) * fs
+
+    # Get the start and end samples
+    firstSamp, lastSamp = sglxr.sglxarray._get_first_and_last_samples(
+        meta, firstSamp, lastSamp
+    )
+
+    # Get timestamps of each sample
+    time, timedelta, datetime = sglxr.sglxarray._get_timestamps(
+        meta, firstSamp, lastSamp
+    )
+
+    # Make memory map to selected data.
+    rawData = makeMemMapRaw(bin_path, meta)
+    selectData = rawData[channels, firstSamp : lastSamp + 1]
+
+    # Apply gain correction and convert to V
+    assert (
+        meta["typeThis"] == "nidq"
+    ), "This function only supports loading of analog NIDQ data."
+    assert all(
+        is_channel_XA(meta, ch) for ch in channels
+    ), "This function only supports loading of analog NIDQ data."
+    sig = 1e3 * GainCorrectNI(selectData, channels, meta)
+    sig_units = "mV"
+
+    # Wrap data with xarray
+    return xr.DataArray(
+        sig.T,
+        dims=("time", "channel"),
+        coords={
+            "time": time,
+            "channel": channels,
+            "timedelta": ("time", timedelta),
+            "datetime": ("time", datetime),
+        },
+        attrs={"units": sig_units, "fs": fs},
+    )
