fcd476b0f0cc1c5530212be9ea96a07060d1708c
Graham Findlay <graham.b.findlay@gmail.com>
2022-04-11 16:55:52 -0500

    use local sglxarray

--- ecephys/version.py
+++ ecephys/version.py
@@ -74,11 +74,11 @@ INSTALL_REQUIRES = [
     "bg-atlasapi",
     "neurodsp",
     "ripple_detection",
-    "spikeinterface",  #  @ git+https://github.com/CSC-UW/spikeinterface.git@master#egg=spikeinterface
+    "spikeinterface",
     "kcsd @ git+https://github.com/Neuroinflab/kCSD-python@master#egg=kcsd",
     "emg_from_lfp @ git+https://github.com/CSC-UW/emg_from_lfp@master#egg=emg_from_lfp",
     "hypnogram @ git+https://github.com/CSC-UW/hypnogram.git@master#egg=hypnogram",
-    "sglxarray @ git+https://github.com/CSC-UW/sglxarray.git@main#egg=sglxarray",
+    "sglxarray",
 ]
 
 PYTHON_REQUIRES = ">= 3.10"
