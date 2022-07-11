6bcee9d53886abba489aae78ba76751cd38c9410
Graham Findlay <graham.b.findlay@gmail.com>
2022-07-11 14:15:35 -0500

    Add new Sorting class methods

--- ecephys/units/sorting.py
+++ ecephys/units/sorting.py
@@ -40,21 +40,35 @@ class SingleProbeSorting(Sorting):
     def n_units(self):
         return len(self.units)
 
+    @property
+    def structures_by_depth(self):
+        return self.units.sort_values("depth").structure.unique().tolist()
+
     def copy(self):
         return SingleProbeSorting(
             self.spikes.copy(),
             self.units.copy(),
         )
-    
+
     def subset_units(self, cluster_ids_to_keep):
         self.spikes = self.spikes.loc[
-            self.spikes['cluster_id'].isin(sorted(cluster_ids_to_keep))
+            self.spikes["cluster_id"].isin(sorted(cluster_ids_to_keep))
         ]
         self.units = self.units.loc[sorted(cluster_ids_to_keep)]
 
+    def filter_trains_by_structure(self, desired_structures):
+        """Given a list of desired structures, return only trains belonging to those structures."""
+        trains = self.spikes.spikes.as_trains()
+        cluster_structures = self.units["structure"]
+        labeled_trains = trains.join(cluster_structures)
+        is_desired = np.isin(labeled_trains["structure"], desired_structures)
+        return labeled_trains[is_desired]
+
     # TODO: There's probably ways to make this faster
     # TODO: Aggregate other columns when units-to-train merge is not many-to-1
-    def get_spike_trains_for_plotting(self, start_time=-float('Inf'), end_time=float('Inf'), grouping_col='cluster_id'):
+    def get_spike_trains_for_plotting(
+        self, start_time=-float("Inf"), end_time=float("Inf"), grouping_col="cluster_id"
+    ):
         if grouping_col not in self.spikes:
             # Add groupby column from units to spikes data
             spikes = self.spikes.spikes.join_units(
@@ -67,15 +81,19 @@ class SingleProbeSorting(Sorting):
             spikes = self.spikes
 
         # Turn depth column to categorical so we represent "empty" depths when grouping as trains
-        if grouping_col == 'depth':
+        if grouping_col == "depth":
             DF_DEPTH_STEP = 20
-            observed_depths = spikes.depth.values 
+            observed_depths = spikes.depth.values
             tmp = np.diff(np.sort(spikes.depth.values))
-            observed_depth_step = tmp[tmp>0].min()
+            observed_depth_step = tmp[tmp > 0].min()
             depth_step = min(DF_DEPTH_STEP, observed_depth_step)
-            depth_categories = np.arange(min(observed_depths), max(observed_depths) + depth_step, depth_step)
-            spikes['depth'] = spikes['depth'].astype("category")
-            spikes['depth'] = spikes['depth'].cat.set_categories(depth_categories, ordered=True)
+            depth_categories = np.arange(
+                min(observed_depths), max(observed_depths) + depth_step, depth_step
+            )
+            spikes["depth"] = spikes["depth"].astype("category")
+            spikes["depth"] = spikes["depth"].cat.set_categories(
+                depth_categories, ordered=True
+            )
 
         trains = spikes.spikes.as_trains(
             start_time=start_time,
@@ -84,7 +102,7 @@ class SingleProbeSorting(Sorting):
         )
 
         # Add all other columns from units after turning into trains
-        if grouping_col == 'cluster_id':
+        if grouping_col == "cluster_id":
             trains = trains.trains.join_units(self.units)
 
         if "rgba" not in trains.columns:
@@ -119,17 +137,35 @@ class MultiProbeSorting(Sorting):
     def n_units(self):
         return sum(len(self.sorts[probe].units) for probe in self.sorts)
 
+    @property
+    def structures_by_depth(self):
+        return {
+            probe: sorting.structures_by_depth for probe, sorting in self.sorts.items()
+        }
+
     def copy(self):
-        return MultiProbeSorting({
-            prb: sorting.copy()
-            for prb, sorting in self.sorts.items()
-        })
+        return MultiProbeSorting(
+            {prb: sorting.copy() for prb, sorting in self.sorts.items()}
+        )
 
     def subset_units(self, cluster_ids_to_keep_per_probe):
         for probe, cluster_ids_to_keep in cluster_ids_to_keep_per_probe.items():
             self.sorts[probe].subset_units(cluster_ids_to_keep)
 
-    def get_spike_trains_for_plotting(self, start_time=-float('Inf'), end_time=float('Inf'), grouping_col='cluster_id'):
+    def filter_trains_by_structure(self, desired_structures):
+        """Takes a dict where each key is a probe (e.g. 'imec0'), and each value is the list of structures you want trains from."""
+        return pd.concat(
+            [
+                self.sorts[probe].filter_trains_by_structure(structures)
+                for probe, structures in desired_structures.items()
+            ],
+            keys=desired_structures.keys(),
+            names=["probe"],
+        ).reset_index()
+
+    def get_spike_trains_for_plotting(
+        self, start_time=-float("Inf"), end_time=float("Inf"), grouping_col="cluster_id"
+    ):
         start_time = self.data_start if start_time is None else start_time
         end_time = self.data_end if end_time is None else end_time
 
