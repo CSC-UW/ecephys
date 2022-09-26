724694d522fc1a78af036d54cff7ee434311c3f6
Graham Findlay <graham.b.findlay@gmail.com>
2022-09-26 10:33:13 -0500

    Remove silent train handling. Something has changed (idk what), such that silent trains are already omitted from the trains dataframe

--- ecephys/units/sorting.py
+++ ecephys/units/sorting.py
@@ -69,7 +69,7 @@ class SingleProbeSorting(Sorting):
     # TODO: There's probably ways to make this faster
     # TODO: Aggregate other columns when units-to-train merge is not many-to-1
     def get_spike_trains_for_plotting(
-        self, start_time=-float("Inf"), end_time=float("Inf"), grouping_col="cluster_id"
+        self, start_time=-float("Inf"), end_time=float("Inf"), grouping_col="cluster_id", 
     ):
         if grouping_col not in self.spikes:
             # Add groupby column from units to spikes data
@@ -109,16 +109,6 @@ class SingleProbeSorting(Sorting):
 
         if "rgba" not in trains.columns:
             set_uniform_rgba(trains, "black")
-
-        silent = trains.trains.silent()
-        # Add ghost spikes at very start and end of window to silent trains, to reserve space for them on the plot's x and y axes.
-        trains.loc[silent, "t"] = pd.Series(
-            [np.array((start_time, end_time))] * sum(silent), dtype="float64"
-        ).values
-        # Make silent units white and transparent, so that they are invisible.
-        trains.loc[silent, "rgba"] = pd.Series(
-            [to_rgba("white", 0.0)] * sum(silent), dtype="object"
-        ).values
 
         return trains.sort_values("depth")
 
