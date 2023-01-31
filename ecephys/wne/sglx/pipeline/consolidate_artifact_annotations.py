dc8e341d6979373a001f35a159457dc1f45c56ef
Graham Findlay <graham.b.findlay@gmail.com>
2023-01-31 17:02:42 -0600

    Update artifact file format

--- ecephys/wne/sglx/pipeline/consolidate_artifact_annotations.py
+++ ecephys/wne/sglx/pipeline/consolidate_artifact_annotations.py
@@ -2,36 +2,40 @@ import logging
 import pandas as pd
 import ecephys as ece
 from tqdm.auto import tqdm
-from ecephys import hypnogram as hg
 
 logger = logging.getLogger(__name__)
 
 
-def do_alias(srcProject, destProject, wneSubject, experiment, alias, probe):
+def do_probe(wneProject, wneSubject, experiment, probe):
     artifacts = list()
-    lfpTable = wneSubject.get_lfp_bin_table(experiment, alias, probe=probe)
-    for lfpFile in tqdm(list(lfpTable.itertuples())):
-        [artFile] = srcProject.get_sglx_counterparts(
+    ftab = wneSubject.get_lfp_bin_table(experiment, probe=probe)
+    for ix, lfpfile in tqdm(list(ftab.iterrows())):
+        [artfile] = wneProject.get_sglx_counterparts(
             wneSubject.name,
-            [lfpFile.path],
+            [lfpfile["path"]],
             ece.wne.constants.ARTIFACTS_EXT,
-            remove_stream=True,
+            remove_stream=True,  # TODO: It turns out artifacts can be stream specific, so these files should be too.
         )
-        logger.debug(f"Looking for file {artFile.name}")
-        if not artFile.is_file():
+        logger.debug(f"Looking for file {artfile.name}")
+        if not artfile.is_file():
             logger.debug("File not found.")
             continue
-        df = pd.read_csv(artFile)
-        df["duration"] = df.apply(lambda row: row.end_time - row.start_time, axis=1)
-        df["state"] = "Artifact"
-        df[["start_time", "end_time"]] += lfpFile[
-            "expmtPrbAcqFirstTime"
-        ]  # TODO: Sync times BEFORE saving
+        df = pd.read_csv(artfile)
+        df["fname"] = lfpfile["path"].name
+        # TODO: Sync to canonical timebase BEFORE saving
+        df["expmtPrbAcqFirstTime"] = df["start_time"] + lfpfile["expmtPrbAcqFirstTime"]
+        df["expmtPrbAcqLastTime"] = df["end_time"] + lfpfile["expmtPrbAcqFirstTime"]
         artifacts.append(df)
 
     if artifacts:
-        df = hg.FloatHypnogram(pd.concat(artifacts, ignore_index=True))
-        artFile = destProject.get_alias_subject_file(
-            experiment, alias, wneSubject.name, ece.wne.constants.ARTIFACTS_FNAME
+        df = pd.concat(artifacts, ignore_index=True)
+        outfile = wneProject.get_experiment_subject_file(
+            experiment, wneSubject.name, f"{probe}.{ece.wne.constants.ARTIFACTS_FNAME}"
         )
-        df.write_htsv(artFile)
+        ece.utils.write_htsv(df, outfile)
+
+
+def do_experiment(wneProject, wneSubject, experiment, probes=None):
+    probes = wneSubject.get_experiment_probes(experiment) if probes is None else probes
+    for probe in probes:
+        do_probe(wneProject, wneSubject, experiment, probe)
