52fee4c44da1b66f720d61198171f9c27f68bea9
Tom Bugnon <tombugnon@hotmail.com>
2023-05-04 11:22:04 -0500

    Black

--- ecephys/units/ephyviewerutils.py
+++ ecephys/units/ephyviewerutils.py
@@ -9,11 +9,14 @@ DEPTH_STEP = 20
 
 
 def add_epochviewer_to_window(
-    window: ephyviewer.MainViewer, events_df: pd.DataFrame, view_name="Events", state_colors=None,
+    window: ephyviewer.MainViewer,
+    events_df: pd.DataFrame,
+    view_name="Events",
+    state_colors=None,
     add_event_list=True,
 ):
     """Add epoch/event view from frame of bouts.
-    
+
     Parameters:
     window: ephyviewer.MainViewer
     event_df: pd.DataFrame
@@ -26,12 +29,14 @@ def add_epochviewer_to_window(
 
     for state in all_states:
         mask = events_df["state"] == state
-        all_epochs.append({
-            "name": state,
-            "label": np.array([state for _ in range(mask.sum())]),
-            "time": events_df[mask]["start_time"].values,
-            "duration": events_df[mask]["duration"].values,
-        })
+        all_epochs.append(
+            {
+                "name": state,
+                "label": np.array([state for _ in range(mask.sum())]),
+                "time": events_df[mask]["start_time"].values,
+                "duration": events_df[mask]["duration"].values,
+            }
+        )
 
     source_epochs = ephyviewer.InMemoryEpochSource(all_epochs=all_epochs)
     view = ephyviewer.EpochViewer(source=source_epochs, name=view_name)
@@ -39,30 +44,34 @@ def add_epochviewer_to_window(
     # Set usual colors
     if state_colors:
         for i, state in enumerate(all_states):
-            view.by_channel_params[f"ch{i}", 'color'] = matplotlib.colors.rgb2hex(state_colors[state])
+            view.by_channel_params[f"ch{i}", "color"] = matplotlib.colors.rgb2hex(
+                state_colors[state]
+            )
 
     window.add_view(view, location="bottom", orientation="vertical")
 
     if add_event_list:
         # Add event list for navigation
         view = ephyviewer.EventList(source=source_epochs, name=f"{view_name} list")
-        window.add_view(view, location='bottom',  orientation='horizontal')
+        window.add_view(view, location="bottom", orientation="horizontal")
 
     return window
 
 
 def add_spiketrainviewer_to_window(
-        window: ephyviewer.MainViewer, sorting, by="cluster_id", tgt_struct_acronym=None, probe=None,
-        view_params=None,
-    ):
+    window: ephyviewer.MainViewer,
+    sorting,
+    by="cluster_id",
+    tgt_struct_acronym=None,
+    probe=None,
+    view_params=None,
+):
 
     properties = sorting.properties
 
     if view_params is None:
-        view_params = {
-            "display_labels": False
-        }
-    
+        view_params = {"display_labels": False}
+
     # Structure-wide information
     if tgt_struct_acronym is not None:
         mask = properties["acronym"] == tgt_struct_acronym
@@ -85,16 +94,11 @@ def add_spiketrainviewer_to_window(
         tgt_values = np.arange(lo, hi + DEPTH_STEP, DEPTH_STEP)[::-1]
     else:
         # Units sorted by depth within each structure
-        sorted_tgt_properties = tgt_properties.sort_values(
-            by="depth", ascending=False
-        )
-        tgt_values = sorted_tgt_properties[by].unique() # Still sorted
+        sorted_tgt_properties = tgt_properties.sort_values(by="depth", ascending=False)
+        tgt_values = sorted_tgt_properties[by].unique()  # Still sorted
 
     all_struct_spikes = []
-    for tgt_value in tqdm(
-        tgt_values,
-        desc=f"Loading spikes for view: `{view_name}`"
-    ):
+    for tgt_value in tqdm(tgt_values, desc=f"Loading spikes for view: `{view_name}`"):
 
         label = f"{by}: {tgt_value}"
 
@@ -102,14 +106,16 @@ def add_spiketrainviewer_to_window(
             ids = tgt_properties[tgt_properties[by] == tgt_value].cluster_id.values
             label = f"{label}, ids={ids}"
 
-        all_struct_spikes.append({
-            'time': sorting.get_trains(
-                by=by,
-                tgt_values=[tgt_value],
-                verbose=False,
-            )[tgt_value],
-            'name': label,
-        })
+        all_struct_spikes.append(
+            {
+                "time": sorting.get_trains(
+                    by=by,
+                    tgt_values=[tgt_value],
+                    verbose=False,
+                )[tgt_value],
+                "name": label,
+            }
+        )
 
     source = ephyviewer.InMemorySpikeSource(all_spikes=all_struct_spikes)
     view = ephyviewer.SpikeTrainViewer(source=source, name=view_name)
