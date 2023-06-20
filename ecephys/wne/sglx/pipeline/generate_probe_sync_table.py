26193ddcd1f056c148f44da2abc85fffa3982ef6
Graham Findlay <graham.b.findlay@gmail.com>
2023-06-20 17:05:38 -0500

    More robust generation of probe sync tables

--- ecephys/wne/sglx/pipeline/generate_probe_sync_table.py
+++ ecephys/wne/sglx/pipeline/generate_probe_sync_table.py
@@ -1,5 +1,7 @@
 import pathlib
+import warnings
 
+import numpy as np
 import pandas as pd
 
 from ecephys import sync
@@ -39,23 +41,40 @@ def get_barcode_sync_table(
         ), "File indices are not matched across probe tables"
     nFiles = len(ftabs["imec0"])
 
-    fits = list()
+    fits = pd.DataFrame()
     for i in range(nFiles):
         imec0BinPath = ftabs["imec0"].iloc[i]["path"]
-        fits.append(
-            pd.DataFrame(
-                {
-                    "source": [imec0BinPath.name],
-                    "target": [imec0BinPath.name],
-                    "slope": [1.0],
-                    "intercept": [0.0],
-                }
-            )
+        _df = pd.DataFrame(
+            {
+                "source": [imec0BinPath.name],
+                "target": [imec0BinPath.name],
+                "source_probe": "imec0",
+                "target_probe": "imec0",
+                "slope": [1.0],
+                "intercept": [0.0],
+            }
         )
+        fits = pd.concat([fits, _df], ignore_index=True)
         for probe in set(probes) - {"imec0"}:
             thisBinPath = ftabs[probe].iloc[i]["path"]
             thisSyncData = load_barcodes(thisBinPath)
             imec0SyncData = load_barcodes(imec0BinPath)
+            if min(len(thisSyncData), len(imec0SyncData)) == 0:
+                warnings.warn(
+                    f"Not enough barcodes to sync {thisBinPath} with {imec0BinPath}. Will attempt to interpolate."
+                )
+                _df = pd.DataFrame(
+                    {
+                        "source": [thisBinPath.name],
+                        "target": [imec0BinPath.name],
+                        "source_probe": probe,
+                        "target_probe": "imec0",
+                        "slope": [np.NaN],
+                        "intercept": [np.NaN],
+                    }
+                )
+                fits = pd.concat([fits, _df], ignore_index=True)
+                continue
 
             fit = sync.fit_barcode_times(
                 thisSyncData["time"].values,
@@ -65,18 +84,29 @@ def get_barcode_sync_table(
                 sysX_name=probe,
                 sysY_name="imec0",
             )
-            fits.append(
-                pd.DataFrame(
-                    {
-                        "source": [thisBinPath.name],
-                        "target": [imec0BinPath.name],
-                        "slope": [fit.coef_[0]],
-                        "intercept": [fit.intercept_],
-                    }
-                )
+            _df = pd.DataFrame(
+                {
+                    "source": [thisBinPath.name],
+                    "target": [imec0BinPath.name],
+                    "source_probe": probe,
+                    "target_probe": "imec0",
+                    "slope": [fit.coef_[0]],
+                    "intercept": [fit.intercept_],
+                }
             )
+            fits = pd.concat([fits, _df], ignore_index=True)
 
-    return pd.concat(fits).drop_duplicates()
+    fits = fits.sort_values(["source_probe", "target_probe"])
+    for probe in set(probes) - {"imec0"}:
+        is_probe = fits["source_probe"] == probe
+        fits.loc[is_probe] = fits.loc[is_probe].interpolate(
+            method="linear", limit=1, limit_direction="both"
+        )
+    if fits["slope"].isna().any() or fits["intercept"].isna().any():
+        raise ValueError(
+            "Unable to interpolate missing sync info. Too much missing data for the given interpolation limit."
+        )
+    return fits
 
 
 def do_experiment(
@@ -89,6 +119,7 @@ def do_experiment(
         experiment, wneSubject.name, constants.EXP_PARAMS_FNAME
     )
     if opts["imSyncType"] != "barcode":
+        # TODO: Implement for single-probe / no-sync recordings [should be covered if implemented for square-pulse]
         raise NotImplementedError("Sync table generation only implemented for barcodes")
 
     sync_table = get_barcode_sync_table(wneProject, wneSubject, experiment, stream)
