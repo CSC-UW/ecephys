ba9c6d76d75b46ace3f91a81e0f18421fbdc76ae
Graham Findlay <graham.b.findlay@gmail.com>
2023-06-28 15:56:34 -0500

    Fix import

--- ecephys/units/__init__.py
+++ ecephys/units/__init__.py
@@ -5,8 +5,8 @@ from ecephys.units.cluster_trains import *
 from ecephys.units.correlograms import *
 from ecephys.units.peths import (
     get_peths_from_trains,
+    get_peths_from_trains_alt,
     get_peths_from_spike_vector,
-    get_peths_from_trains_v2,
 )
 from ecephys.units.siks_sorting import SpikeInterfaceKilosortSorting
 from ecephys.units.multi_siks import MultiSIKS
