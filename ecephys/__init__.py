80da5f81ea13e4e36509cfccce7f19643e77c217
Graham Findlay <graham.b.findlay@gmail.com>
2025-01-27 11:20:54 -0600

    expose toplevel package contents to public api

--- ecephys/__init__.py
+++ ecephys/__init__.py
@@ -2,18 +2,35 @@ __version__ = "0.0.1"  # TODO: Use importlib to get this properly from pyproject
 # The version string is needed by Spikeinterface when serializing and deserializing extractor objects from this library during multiprocessing.
 
 from . import (
-    sharptrack,
     data_mgmt,
     emg_from_lfp,
     hypnogram,
+    npsig,
     plot,
     sglx,
     sglxr,
-    npsig,
-    tdtxr,
+    sharptrack,
     sync,
+    tdtxr,
     units,
     utils,
-    xrsig,
     wne,
+    xrsig,
 )
+
+__all__ = [
+    "sharptrack",
+    "data_mgmt",
+    "emg_from_lfp",
+    "hypnogram",
+    "plot",
+    "sglx",
+    "sglxr",
+    "npsig",
+    "tdtxr",
+    "sync",
+    "units",
+    "utils",
+    "xrsig",
+    "wne",
+]
