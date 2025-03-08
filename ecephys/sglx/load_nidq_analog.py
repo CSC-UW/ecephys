83c66984dc7c64bfb2b798d3d124ee2e6a67e367
Graham Findlay <graham.b.findlay@gmail.com>
2025-03-07 20:50:43 -0600

    Update sglx package import

--- ecephys/sglx/load_nidq_analog.py
+++ ecephys/sglx/load_nidq_analog.py
@@ -3,46 +3,36 @@ from pathlib import Path
 import numpy as np
 import xarray as xr
 
-# TODO: This should be satisfied by sglx.external
-from ecephys.sglxr.external.readSGLX import (
-    ChannelCountsNI,
-    GainCorrectNI,
-    SampRate,
-    makeMemMapRaw,
-    readMeta,
-)
-
 # TODO: sglxr should not be a dependency of sglx
-from ecephys.sglxr.sglxr import (
-    _get_first_and_last_samples,
-    _get_timestamps,
-    _to_seconds_from_file_start,
-)
+from ecephys.sglxr import sglxr
+
+# TODO: This should be satisfied by sglx.external
+from ecephys.sglxr.external import readSGLX
 
 
 def _is_channel_XA(meta, channel):
-    MN, MA, XA, DW = ChannelCountsNI(meta)
+    MN, MA, XA, DW = readSGLX.ChannelCountsNI(meta)
     return (channel >= (MN + MA)) and (channel < XA)
 
 
 def load_nidq_analog(bin_path, channels, start_time=0, end_time=np.Inf):
     # Read and validate the metadata
     bin_path = Path(bin_path)
-    meta = readMeta(bin_path)
+    meta = readSGLX.readMeta(bin_path)
 
     # Get the requested start and end samples
-    fs = SampRate(meta)
-    firstSamp = _to_seconds_from_file_start(start_time, meta) * fs
-    lastSamp = _to_seconds_from_file_start(end_time, meta) * fs
+    fs = readSGLX.SampRate(meta)
+    firstSamp = sglxr._to_seconds_from_file_start(start_time, meta) * fs
+    lastSamp = sglxr._to_seconds_from_file_start(end_time, meta) * fs
 
     # Get the start and end samples
-    firstSamp, lastSamp = _get_first_and_last_samples(meta, firstSamp, lastSamp)
+    firstSamp, lastSamp = sglxr._get_first_and_last_samples(meta, firstSamp, lastSamp)
 
     # Get timestamps of each sample
-    time, datetime, _ = _get_timestamps(meta, firstSamp, lastSamp)
+    time, datetime, _ = sglxr._get_timestamps(meta, firstSamp, lastSamp)
 
     # Make memory map to selected data.
-    rawData = makeMemMapRaw(bin_path, meta)
+    rawData = readSGLX.makeMemMapRaw(bin_path, meta)
     selectData = rawData[channels, firstSamp : lastSamp + 1]
 
     # Apply gain correction and convert to V
@@ -52,7 +42,7 @@ def load_nidq_analog(bin_path, channels, start_time=0, end_time=np.Inf):
     assert all(_is_channel_XA(meta, ch) for ch in channels), (
         "This function only supports loading of analog NIDQ data."
     )
-    sig = 1e3 * GainCorrectNI(selectData, channels, meta)
+    sig = 1e3 * readSGLX.GainCorrectNI(selectData, channels, meta)
     sig_units = "mV"
 
     # Wrap data with xarray
