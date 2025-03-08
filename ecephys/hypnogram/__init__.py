8de35751a5e0c88681ebf84b988ac4f78838e7ef
Graham Findlay <graham.b.findlay@gmail.com>
2025-03-07 19:41:16 -0600

    Update imports and format for hypnogram subpackage

--- ecephys/hypnogram/__init__.py
+++ ecephys/hypnogram/__init__.py
@@ -1,2 +1,33 @@
-from .hypnogram import *
 from . import examples
+from .core import (
+    DatetimeHypnogram,
+    FloatHypnogram,
+    Hypnogram,
+    clean,
+    condense,
+    ffill_gaps,
+    fill_gaps,
+    get_gaps,
+    get_separated_wake_hypnogram,
+    reconcile_hypnograms,
+    remove_subsumed,
+    trim_hypnogram,
+    trim_overlap,
+)
+
+__all__ = [
+    "Hypnogram",
+    "FloatHypnogram",
+    "DatetimeHypnogram",
+    "examples",
+    "get_separated_wake_hypnogram",
+    "reconcile_hypnograms",
+    "remove_subsumed",
+    "condense",
+    "trim_overlap",
+    "get_gaps",
+    "fill_gaps",
+    "ffill_gaps",
+    "trim_hypnogram",
+    "clean",
+]
--- ecephys/hypnogram/examples.py
+++ ecephys/hypnogram/examples.py
@@ -1,15 +1,16 @@
 import os.path as op
-import ecephys.hypnogram as hg
 
-data_path = op.join(hg.__path__[0], "data")
+from . import core
+
+data_path = op.join(core.__path__[0], "data")
 # Load data like: op.join(data_path, 'mydatafile.dat')
 
 
 def float_hypnogram():
     p = op.join(data_path, "visbrain_hypnogram.txt")
-    return hg.load_visbrain_hypnogram(p)
+    return core.load_visbrain_hypnogram(p)
 
 
 def datetime_hypnogram():
     p = op.join(data_path, "datetime_hypnogram.tsv")
-    return hg.load_datetime_hypnogram(p)
+    return core.load_datetime_hypnogram(p)
