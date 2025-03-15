ac5ef76a71339de1c7d4b06d96f182172c70f943
Graham Findlay <graham.b.findlay@gmail.com>
2025-03-14 22:44:31 -0500

    Move some wne.sglx spikeinterface utils out of regular utils.

--- ecephys/wne/sglx/utils.py
+++ ecephys/wne/sglx/utils.py
@@ -1,5 +1,4 @@
 # TODO: This file should probably be broken into separate modules.
-import itertools
 import logging
 import pathlib
 from typing import Callable, Optional
@@ -8,20 +7,15 @@ import numpy as np
 import pandas as pd
 
 import ecephys.utils
-from ecephys import hypnogram as hyp
-from ecephys import units
 from ecephys.sglx import file_mgmt
-from ecephys.wne import constants, siutils
+from ecephys.wne import constants
 from ecephys.wne import utils as wne_utils
-from ecephys.wne.project import Project
 
 from . import sessions
 from .project import SGLXProject
 from .subject import SGLXSubject
 
 logger = logging.getLogger(__name__)
-
-MIN_BOUT_DURATION_SEC = 0.1  # SUS: Why is this a module level constant? Why is it not just a pre-defined parameter? Why not a WNE constant?
 
 
 def get_sglx_file_counterparts(
@@ -62,52 +56,17 @@ def get_sglx_file_counterparts(
     return ecephys.utils.remove_duplicates(counterparts)
 
 
-# TODO: This function needs to be differentiated from load_sglx_inclusions_and_artifacts.
-# When would you use this one, and when would you use the other?
-def load_sorting_inclusions_and_artifacts(
-    t2t,
-    project,
-    sglx_subject,
-    experiment,
-    probe,
-    alias,
-    sorting,
-):
-    # Query inclusions from segments used in actual sorting
-    # segments are in probe timebase and need to be converted to common timebase
-    segments = project.load_segments_table(
-        sglx_subject.name,
-        experiment,
-        alias,
-        probe,
-        sorting,
-        return_all_segment_types=True,
-    ).copy()
-    segments = pd.DataFrame(
-        {
-            "start_time": t2t(segments["segmentExpmtPrbAcqFirstTime"]),
-            "end_time": t2t(segments["segmentExpmtPrbAcqLastTime"]),
-            "type": segments["type"],
-        }
-    )  # Raw segment table is in probe timebase
-
-    inclusions = segments.loc[segments["type"] == "keep"]
-    artifacts = inclusions.iloc[:0].copy()  # Dummy
-
-    return inclusions, artifacts
-
-
 # TODO: This function needs to be differentiated from load_sorting_inclusions_and_artifacts.
 # When would you use this one, and when would you use the other?
 def load_sglx_inclusions_and_artifacts(
-    t2t,
-    project,
-    sglx_subject,
-    experiment,
-    probe,
-    alias,
-    stream,
-):
+    t2t: Callable,
+    project: SGLXProject,
+    sglx_subject: SGLXSubject,
+    experiment: str,
+    probe: str,
+    alias: str,
+    stream: str,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
     if alias != "full":
         raise NotImplementedError("Restrict artifacts to alias")
 
@@ -134,276 +93,6 @@ def load_sglx_inclusions_and_artifacts(
     )
 
     return inclusions, artifacts
-
-
-def _get_gaps(
-    df,
-    t1_colname: str = "start_time",
-    t2_colname: str = "end_time",
-    min_gap_duration_sec: float = 0,
-):
-    gaps = pd.DataFrame(
-        {
-            t1_colname: df.iloc[:-1][t2_colname].values,
-            t2_colname: df.iloc[1:][t1_colname].values,
-        }
-    )
-    gaps["duration"] = gaps[t2_colname] - gaps[t1_colname]
-    return gaps[gaps["duration"] > min_gap_duration_sec]
-
-
-# SUS: This function's name does not appear to describe what it does: return all the NoData and/or artifactual periods from a probe.
-def load_bouts_to_reconcile_as_hypnogram(
-    project: SGLXProject,
-    experiment: str,
-    sglx_subject: SGLXSubject,
-    probe: str,
-    source: str,  # TODO: Currently, you have to check first if the source exists. This function should probably do that for you.
-    alias: str = "full",
-    sorting: str = "sorting",
-    min_bout_duration_sec=MIN_BOUT_DURATION_SEC,
-) -> hyp.FloatHypnogram:
-    if source in ["sorting", "ap"]:
-        stream = "ap"
-    elif source == "lf":
-        stream = "lf"
-    else:
-        raise ValueError(f"Invalid source: {source}")
-
-    # Convert from probe timebase to common timebase asap
-    t2t = get_time_synchronizer(
-        project,
-        sglx_subject,
-        experiment,
-        probe=probe,
-        stream=stream,
-    )
-
-    if source == "sorting":
-        inclusions, artifacts = load_sorting_inclusions_and_artifacts(
-            t2t,
-            project,
-            sglx_subject,
-            experiment,
-            probe,
-            alias,
-            sorting,
-        )
-
-    elif source in ["lf", "ap"]:
-        inclusions, artifacts = load_sglx_inclusions_and_artifacts(
-            t2t,
-            project,
-            sglx_subject,
-            experiment,
-            probe,
-            alias,
-            stream,
-        )
-
-    # Infer "NoData" hypnogram from inclusions
-    # 1-sample imprecision
-    no_data = _get_gaps(
-        inclusions,
-        t1_colname="start_time",
-        t2_colname="end_time",
-    )
-    no_data["state"] = "NoData"
-    no_data_hg = hyp.FloatHypnogram(no_data)
-
-    # Get "artifacts" hypnogram: "type" column now becomes "state"
-    artifacts = artifacts.rename(columns={"type": "state"})
-    artifacts["duration"] = artifacts["end_time"] - artifacts["start_time"]
-    artifacts_hg = hyp.FloatHypnogram(artifacts)
-
-    # Reconcile NoData & artifacts
-    return hyp.FloatHypnogram(
-        no_data_hg.reconcile(artifacts_hg, how="other")
-        .keep_longer(min_bout_duration_sec)
-        .reset_index(drop=True)
-    )
-
-
-def load_reconciled_float_hypnogram(
-    project: Project,
-    experiment: str,
-    sglx_subject: SGLXSubject,
-    probes: list[str],
-    sources: list[str],
-    reconcile_ephyviewer_edits: bool = True,
-    simplify: bool = True,
-    alias="full",
-    sorting="sorting",
-) -> hyp.FloatHypnogram:
-    """Load FloatHypnogram reconciled with LF/AP/sorting artifacts & NoData.
-
-    Favor using this function, rather than load_raw_float_hypnogram, for
-    actual analyses! It ensures that :
-        - the probes' actual NoData bouts and
-        - the probes' artifacts
-        - Manual edits made post-hoc in ephyviewer
-    are incorporated
-
-    This is not guaranteed to be the case with the load_raw_float_hypnogram,
-    in particular since:
-        - SGLX files and artifacts may vary across streams/probes
-        - Some bouts may have been excluded from a sorting # TODO: Why?
-
-    # If probes and sources are not passed (e.g. empty lists), this function effectively is just for loading the ephyviewer edits and/or cleaning.
-
-    Parameters:
-    ===========
-    project: Project
-        Used to load sorting segments, sync table, and LF artifacts
-    experiment: str
-    subject: SGLXSubject
-    probes: list[str]
-        Probes for which we load bouts to reconcile with raw hypnogram
-    sources: list[str]
-        Sources must be one of ["ap", "lf", "sorting"].
-        For "lf" and "ap" source, the NoData bouts are inferred from the sglx filetable,
-        and the artifacts are loaded from the project's default consolidated
-        artifact file.  For "sorting" source, NoData bouts are loaded from the
-        sorting segments table.
-        # TODO: How are these related? Is the sorting segments table always a superset of the consolidated artifact file?
-    simplify: bool
-        Passed to load_raw_float_hypnogram. Simplifies states from raw float hypnogram
-    alias: str
-        Alias used for sorting. Used only when querying "sorting" source.
-    sorting: str
-        Name of sorting. Used only when querying "sorting" source
-    """
-    SOURCES = ["sorting", "ap", "lf"]
-    if not set(sources) <= set(SOURCES):
-        raise ValueError(
-            f"Invalid value in `sources` argument. The following sources are recognized: `{SOURCES}`"
-        )  # TODO: This is not true. As written, sources=[] will also pass this test. Is that intended, or a bug?
-    hg = wne_utils.load_raw_float_hypnogram(
-        project,
-        experiment,
-        sglx_subject.name,
-        simplify=simplify,
-    )
-    if reconcile_ephyviewer_edits:
-        hg = hg.reconcile(
-            wne_utils.load_ephyviewer_hypnogram_edits(
-                project, experiment, sglx_subject.name, simplify=simplify
-            ),
-            how="other",
-        )
-
-    # Only keep bouts that are artifact-free and have data on EVERY probe. The "most conservative" hypnogram, if you will.
-    # Any period covered by this hypnogram is guaranteed to be artifact-free and have data on every probe.
-    # SUS: If either sources or probes is an empty list, this for-loop will be bypassed entirely. Is that intended?
-    for source, probe in itertools.product(sources, probes):
-        hg = hg.reconcile(
-            load_bouts_to_reconcile_as_hypnogram(
-                project,
-                experiment,
-                sglx_subject,
-                probe,
-                source,
-                alias=alias,
-                sorting=sorting,
-            ),
-            how="other",
-        )
-
-    return hyp.FloatHypnogram.clean(hg.reset_index(drop=True))
-
-
-def load_singleprobe_sorting(
-    sglxSortingProject: SGLXProject,
-    sglxSubject: SGLXSubject,
-    experiment: str,
-    probe: str,
-    alias: str = "full",
-    sorting: str = "sorting",
-    postprocessing: str = "postpro",
-    wneAnatomyProject: Optional[Project] = None,
-    allow_no_sync_file=False,
-) -> units.SpikeInterfaceKilosortSorting:
-    if sorting is None:
-        sorting = "sorting"
-    if postprocessing is None:
-        postprocessing = "postpro"
-
-    # Get function for converting SI samples to imec0 timebase
-    sample2time = sglxSortingProject.get_sample2time(
-        sglxSubject.name,
-        experiment,
-        alias,
-        probe,
-        sorting,
-        allow_no_sync_file=allow_no_sync_file,
-    )
-
-    # Load extractor
-    extractor = sglxSortingProject.get_kilosort_extractor(
-        sglxSubject.name,
-        experiment,
-        probe,
-        alias=alias,
-        sorting=sorting,
-        postprocessing=postprocessing,
-    )
-
-    # TODO: Why was this removed, and should it be restored?
-    # extractor = units.si_ks_sorting.fix_isi_violations_ratio(extractor)
-
-    # Add anatomy to the extractor, if available.
-    if wneAnatomyProject is None:
-        wneAnatomyProject = sglxSortingProject
-
-    anatomy_file = wneAnatomyProject.get_experiment_subject_file(
-        experiment, sglxSubject.name, f"{probe}.structures.htsv"
-    )
-    if anatomy_file.exists():
-        structs = ecephys.utils.read_htsv(anatomy_file)
-    else:
-        import warnings
-
-        warnings.warn(
-            "Could not find anatomy file at: {anatomy_file}. Using dummy structure table"
-        )
-        structs = siutils.get_dummy_structure_table(lo=-np.inf, hi=np.inf)
-    extractor = siutils.add_anatomy_properties_to_extractor(extractor, structs)
-
-    return units.SpikeInterfaceKilosortSorting(extractor, sample2time)
-
-
-def load_multiprobe_sorting(
-    sglxSortingProject: SGLXProject,
-    sglxSubject: SGLXSubject,
-    experiment: str,
-    probes: list[str],
-    alias: str = "full",
-    sortings: dict[str, str] = None,
-    postprocessings: dict[str, str] = None,
-    wneAnatomyProject: Optional[Project] = None,
-    allow_no_sync_file=False,
-) -> units.MultiSIKS:
-    if sortings is None:
-        sortings = {prb: None for prb in probes}
-    if postprocessings is None:
-        postprocessings = {prb: None for prb in probes}
-
-    return units.MultiSIKS(
-        {
-            probe: load_singleprobe_sorting(
-                sglxSortingProject,
-                sglxSubject,
-                experiment,
-                probe=probe,
-                alias=alias,
-                sorting=sortings[probe],
-                postprocessing=postprocessings[probe],
-                wneAnatomyProject=wneAnatomyProject,
-                allow_no_sync_file=allow_no_sync_file,
-            )
-            for probe in probes
-        }
-    )
 
 
 def get_experiment_sample2time(
