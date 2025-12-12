72495a6864eae9974fa266d3154d03df0d341b2f
Graham Findlay <graham.b.findlay@gmail.com>
2025-12-11 20:21:09 -0600

    Move _get_recording() and _create_slice_table_for_spikeinterface() to lsnp.deprecated

--- ecephys/wne/sglx/legacy_sorting.py
+++ ecephys/wne/sglx/legacy_sorting.py
@@ -8,123 +8,15 @@ from typing import Callable, Optional
 
 import numpy as np
 import pandas as pd
-import spikeinterface as si
-import spikeinterface.extractors as se
 
 import ecephys.utils
 from ecephys import units, wne
 from ecephys.wne.sglx.project import SGLXProject
-from ecephys.wne.sglx.subject import SGLXSubject
 
 logger = logging.getLogger(__name__)
 
 
-def _create_slice_table_for_spikeinterface(
-    subject_ftab: pd.DataFrame, exclusions: pd.DataFrame
-) -> pd.DataFrame:
-    df = wne.sglx.utils.create_slice_table_for_spikeinterface(
-        subject_ftab, exclusions, return_dropped_slices=True
-    )
-    df = df.rename(
-        columns={"sliceType": "type"}
-    )  # Rename back for legacy compatibility.
-
-    # TODO: Remove, as these can (1) be derived later, and (2) get overwritten in
-    #       load_slice_table_from_sorting_folder().
-    df["nSegmentSamp"] = df["withinFileEndFrame"] - df["withinFileStartFrame"]
-    df["segmentDuration"] = df["nSegmentSamp"].div(df["imSampRate"])
-
-    return df
-
-
 # Was `SGLXProject.get_si_recording()`
-# TODO: I would make this a private function, to discourage its use.
-def get_recording(
-    subject: SGLXSubject,
-    experiment: str,
-    alias: str,
-    stream: str,
-    probe: str,
-    combine: str = "concatenate",
-    exclusions: Optional[pd.DataFrame] = None,
-    sampling_frequency_max_diff: Optional[float] = 1e-6,
-) -> tuple[si.BaseRecording, pd.DataFrame]:
-    """Combine the one or more recordings comprising an experiment or alias into a single SI recording object.
-
-    Parameters
-    ==========
-    combine: 'concatenate' or 'append'
-        If 'concatenate' (default), the returned recording object is one single monolothic segment.
-        This is the default behavior, because SI sorters currently only work on single-segment recordings.
-        If 'append', the returned recording object consists of multiple segments.
-        This might be useful for certain preprocessing, postprocessing operations, etc.
-    exclusions:
-        Specify which parts of the recording to drop. We slice such that the first and last samples
-        of each exclusion are NOT included in the returned recording.
-        fname: The name of the file (e.g. 3-2-2021_J_g0_t1.imec1.ap.bin)
-        withinFileStartTime: The start time of the data to drop, in seconds from the start of the file.
-        withinFileEndTime: The end time of the data to drop, in seconds from the start of the file.
-            If greater than the file duration, the excess time will be ignored, not dropped from the next file.
-        type: A label you can assign to keep track of why this data was excluded. As long as the value is not "keep", the data will be dropped.
-
-    Returns
-    =======
-    recording:
-        The combined SI recording object.
-    exclusions:
-        A dataframe where each row is a slice of data to keep, or drop, sorted in chronological order.
-            fname: The name of the file (e.g. 3-2-2021_J_g0_t1.imec1.ap.bin)
-            withinFileStartFrame: The first sample index of the slice, measured from the start of the file (0-indexed)
-            withinFileEndFrame: The final sample index of the slice, measured from the start of the file (0-indexed)
-            type: Either 'keep', in which case the slice was kept, or other, in which case the slice was dropped.
-            segmentDuration: Duration in sec of slice.
-    """
-    # TODO: Instead of anticipating SI segment indices and adding them to the ftab at the start,
-    # we should use the neo header in the SI extractor to add the segment indices to an ftab,
-    # or to our slice table.
-    #
-    # Get the experiment frame. This should be for a single probe, and a single stream.
-    ftab = subject.get_experiment_frame(
-        experiment, alias=alias, stream=stream, ftype="bin", probe=probe
-    )
-    # Split the experiment frame around the exclusions, using precise sample indices.
-    if exclusions is None:
-        exclusions = wne.utils.get_dummy_artifacts_table()
-    slices = _create_slice_table_for_spikeinterface(
-        ftab, exclusions
-    )  # These are NOT the segments of a SpikeInterface recording!
-
-    # Take the good slices one by one, create an recording object for each, and save these all in a list
-    good_slices = slices[slices["type"] == "keep"]
-    recordings = list()
-    for slice_ in good_slices.itertuples():
-        extractor = se.SpikeGLXRecordingExtractor(
-            slice_.gate_dir.parent, stream_id=f"{probe}.{stream}"
-        )  # slice_.gate_dir.parent is the actual gate directory.
-        recording = extractor.select_segments(
-            [slice_.gate_dir_trigger_file_idx]
-        ).frame_slice(
-            start_frame=slice_.withinFileStartFrame,
-            end_frame=slice_.withinFileEndFrame,
-        )
-        recordings.append(recording)
-
-    if combine == "concatenate":
-        recording = si.concatenate_recordings(
-            recordings, sampling_frequency_max_diff=sampling_frequency_max_diff
-        )
-    elif combine == "append":
-        recording = si.append_recordings(
-            recordings, sampling_frequency_max_diff=sampling_frequency_max_diff
-        )
-    else:
-        raise ValueError(f"Got unexpected value for `combine`: {combine}")
-
-    # We return both recording and slices together, rather than making the available separately,
-    # to ensure that you never get a slice table unless it is actually proven to produce a valid extractor object.
-    return recording, slices
-
-
 # TODO: Consider making this a method on wne.Project, or at least a function in wne.sorting
 # Although, I don't really like the idea that it uses the alias. But the existing data could be moved to eliminate the alias.
 def get_sorting_directory(
